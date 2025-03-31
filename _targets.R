c0511c2621cec81422c7089341354f312bf65da1
Jameel Alsalam <alsalam.jameel@epa.gov>
2025-03-31 16:44:28 -0400

    so getwd() always in subdir

--- _targets.R
+++ _targets.R
@@ -6,6 +6,6 @@ library(tarchetypes)
 
 list(
   tar_target(data, palmerpenguins::penguins),
-  tar_quarto(book1, "book-1"),
-  tar_quarto(book2, "book-2")
+  tar_quarto(book1, "book-1", working_directory = "book-1"),
+  tar_quarto(book2, "book-2", working_directory = "book-2")
 )
